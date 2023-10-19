63eef9f14d297e8501306eeb63db3981c2f0b21f
Constantin Cutureanu <constantin.cutureanu@gmd.local>
2023-10-19 12:23:35 +0300

    done task 4 and modified file README.md

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreareArray(int count)
+{
+    string[] UserArray = new string[count];
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"Input line {i + 1}: ");
+        UserArray[i] = Console.ReadLine();
+    }
+    return UserArray;
+}
+
+void WriteArray(string[] array)
+{ 
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"\"{array[i]}\"");
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write(" ]");
+    
+    // ну или так :D
+    // string result = "[ " + string.Join(", ", array.Select(s => $"\"{s}\"")) + " ]";
+    // Console.Write(result);
+    
+}
+
+string[] FilterArray(string[] Array)
+{
+    string[] doneArray = new string[Array.Length];
+
+    int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        string str = Array[i];
+
+        if (str.Length <= 3)
+        {
+            doneArray[count] = str;
+            count++;
+        }
+    }
+    string[] finalArray = new string[count]; // удаление пустых строк [как вы и просили, массивом все :( ]
+    for (int i = 0; i < count; i++)
+    {
+        finalArray[i] = doneArray[i];
+    }
+    return finalArray;
+}
+
+Console.Write("Input the count of lines: ");
+while (true)
+{
+    int count;
+
+    if (int.TryParse(Console.ReadLine(), out count))
+    {
+        string[] ResultArray = CreareArray(count);
+        Console.WriteLine();
+        WriteArray(ResultArray);
+
+        string[] ResultFilterArray = FilterArray(ResultArray);
+        Console.Write(" -> ");
+        WriteArray(ResultFilterArray);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid number( ");
+        Console.WriteLine();
+    }
+}
